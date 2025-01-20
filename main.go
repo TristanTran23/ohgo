aa10c8f9a34e37a93959649aa55508feb0caa0e0
TristanTran23 <tranquangthinhtdn@gmail.com>
2025-01-20 15:28:41 -0500

    nth much

--- main.go
+++ main.go
@@ -21,4 +21,48 @@ type Camera struct {
   URL string `json:"url"`
 }
 
+type Location struct {
+  Latitude float64 `json:"latitude"`
+  Longitude float64 `json:"longitude"`
+}
 
+type Client struct {
+  baseURL string
+  httpClient *http.Client
+}
+
+func NewClient() *Client {
+  return &Client{
+    baseURL: "https://api.ohio.gov/v1",
+    httpClient: &http.Client{
+      Timeout: time.Second * 30,
+    },
+  }
+}
+
+func (c *Client) GetCameras() ([]Camera, error) {
+  resp, err := c.httpClient.Get(fmt.Sprintf("%s/cameras", c.baseURL))
+  if err != nil {
+    return nil, fmt.Errorf("failed to get cameras: %w", err)
+  }
+  defer resp.Body.Close()
+
+  if resp.StatusCode != http.StatusOK {
+    return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+  }
+  
+  var cameras []Camera
+    if err := json.NewDecoder(resp.Body).Decode(&cameras); err != nil {
+    return nil, fmt.Errorf("failed to decode cameras: %w", err)
+  }
+
+  return cameras, nil
+}
+
+func (c *Client) GetCamraImage(camera *Camera) ([]byte, error) {
+  resp, err := c.httpClient.Get(camera.URL)
+  if err != nil {
+    return nil, fmt.Errorf("failed to get camera image: %w", err)
+  }
+
+}
